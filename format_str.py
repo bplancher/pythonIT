9f8de9a7e9f4a826a5bebfaa6a360ec836f9ee99
plancher bruno <bruno.plancher@gmail.com>
2013-07-01 15:10:52 +0200

    added prog definition of to_upper_unaccent method

--- format_str.py
+++ format_str.py
@@ -1,4 +1,5 @@
 #method to remove accents of a string and upping its cases, it also keep separator char such as spaces or dots
+prog = re.compile('[ _\-:]+')
 def to_upper_unaccent(val):
     uval = val.decode('utf-8')
     separator = prog.search(uval)
