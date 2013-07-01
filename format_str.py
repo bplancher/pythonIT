daf91e0c69707c9437e7ec811f0ea06bc50e03c2
plancher bruno <bruno.plancher@gmail.com>
2013-07-01 15:00:54 +0200

    first commit, first method: to_upper_unaccent

---
+++ format_str.py
@@ -0,0 +1,12 @@
+#method to remove accents of a string and upping its cases, it also keep separator char such as spaces or dots
+def to_upper_unaccent(val):
+    uval = val.decode('utf-8')
+    separator = prog.search(uval)
+    items = [uval]
+    ret = []
+    if separator:
+        items = prog.split(uval)
+    for item in items:
+        ret.append(''.join([x for x in unicodedata.normalize('NFKD',item) if unicodedata.category(x)[0] == 'L']))
+    ret = separator.group(0).join(ret)
+    return ret.upper()
